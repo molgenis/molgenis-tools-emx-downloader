ba30a7d28dc5caa57b628ffd4791a5ba6de840e8
bartcharbon <bart.charbon@gmail.com>
2017-01-18 15:56:37 +0100

    add overwrite option

--- src/main/java/org/molgenis/downloader/emx/tsv/ZipFileBackend.java
+++ src/main/java/org/molgenis/downloader/emx/tsv/ZipFileBackend.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.molgenis.downloader.emx.tsv;
 
 import java.io.IOException;
@@ -11,45 +6,61 @@ import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.molgenis.downloader.api.EMXBackend;
 import org.molgenis.downloader.api.EMXDataStore;
 
+import static org.molgenis.downloader.util.ConsoleWriter.writeToConsole;
+
 /**
- *
  * @author david
  */
-public class ZipFileBackend implements EMXBackend {
+public class ZipFileBackend implements EMXBackend
+{
 
-    private final FileSystem fs;
-    private final List<TSVFile> files;
+	private FileSystem fs;
+	private final List<TSVFile> files;
 
-    public ZipFileBackend(final Path location) throws IOException, URISyntaxException {
-        files = new ArrayList<>();
-        final Map<String, String> env = new HashMap<>();
-        env.put("create", "true");
-        env.put("encoding", StandardCharsets.UTF_8.toString());
-        final URI uri = new URI("jar:file:" + location.toAbsolutePath());
-        fs = FileSystems.newFileSystem(uri, env);
-    }
+	public ZipFileBackend(final Path path, boolean overwrite) throws IOException, URISyntaxException
+	{
+		files = new ArrayList<>();
+		final Map<String, String> env = new HashMap<>();
+		if (Files.exists(path))
+		{
+			if (overwrite) Files.delete(path);
+			else
+			{
+				writeToConsole(String.format("File %s already exists, please use the '-o' option to overwrite.", path));
+				return;
+			}
+		}
+		env.put("create", "true");
+		env.put("encoding", StandardCharsets.UTF_8.toString());
+		final URI uri = new URI("jar:file:" + path.toAbsolutePath());
+		fs = FileSystems.newFileSystem(uri, env);
+	}
 
-    
-    @Override
-    public EMXDataStore createDataStore(final String name) throws IOException {
-        final TSVFile sheet = new TSVFile(fs, name);
-        files.add(sheet);
-        return sheet;
-    }
+	@Override
+	public EMXDataStore createDataStore(final String name) throws IOException
+	{
+		final TSVFile sheet = new TSVFile(fs, name);
+		files.add(sheet);
+		return sheet;
+	}
 
-    @Override
-    public void close() throws Exception {
-        for (TSVFile file: files) {
-            file.close();
-        }
-        fs.close();
-    }   
+	@Override
+	public void close() throws Exception
+	{
+		for (TSVFile file : files)
+		{
+			file.close();
+		}
+		fs.close();
+	}
 }
