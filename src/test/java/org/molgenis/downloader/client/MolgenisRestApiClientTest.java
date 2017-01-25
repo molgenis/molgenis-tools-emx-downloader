31a84b14914fa91d8f069c21af9109433a1d9830
bartcharbon <bart.charbon@gmail.com>
2017-01-25 10:54:58 +0100

    add "getEntity" unittest

--- src/test/java/org/molgenis/downloader/client/MolgenisRestApiClientTest.java
+++ src/test/java/org/molgenis/downloader/client/MolgenisRestApiClientTest.java
@@ -1,8 +1,11 @@
 package org.molgenis.downloader.client;
 
+import com.google.common.collect.Sets;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.molgenis.downloader.api.metadata.Attribute;
+import org.molgenis.downloader.api.metadata.Entity;
 import org.molgenis.downloader.api.metadata.MolgenisVersion;
 import org.testng.annotations.Test;
 
@@ -11,7 +14,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -38,5 +40,30 @@ public class MolgenisRestApiClientTest
 		MolgenisVersion version = client.getVersion();
 		MolgenisVersion expected = new MolgenisVersion(9, 9, 9);
 		assertEquals(version, expected);
+	}
+
+	@Test
+	public void getEntityTest() throws IOException, URISyntaxException
+	{
+		HttpClient httpClient = mock(HttpClient.class);
+		HttpResponse httpResponse = mock(HttpResponse.class);
+		HttpEntity httpEntity = mock(HttpEntity.class);
+
+		File resource = new File(Resources.getResource("entities.txt").getPath());
+		when(httpEntity.getContent()).thenReturn(new FileInputStream(resource));
+		when(httpResponse.getEntity()).thenReturn(httpEntity);
+		when(httpClient.execute(any())).thenReturn(httpResponse);
+
+		MolgenisRestApiClient client = new MolgenisRestApiClient(httpClient, new URI(""));
+		Entity actual = client.getEntity("test");
+
+		Attribute idAttr = new Attribute("id", "id");
+		idAttr.setEntityFullname("biobank");
+		Attribute nameAttr = new Attribute("name", "name");
+		nameAttr.setEntityFullname("biobank");
+		Entity expected = new Entity("biobank");
+		expected.setIdAttribute(idAttr);
+		expected.setAttributes(Sets.newHashSet(idAttr, nameAttr));
+		assertEquals(actual, expected);
 	}
 }
