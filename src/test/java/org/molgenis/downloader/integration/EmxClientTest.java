087c794495c472104a99d73746060de1aa8e1a4f
bartcharbon <bart.charbon@gmail.com>
2017-01-26 08:47:14 +0100

    add integration test

---
+++ src/test/java/org/molgenis/downloader/integration/EmxClientTest.java
@@ -0,0 +1,27 @@
+package org.molgenis.downloader.integration;
+
+import com.google.common.io.Resources;
+import org.apache.http.client.HttpClient;
+import org.molgenis.downloader.client.MolgenisRestApiClient;
+import org.molgenis.downloader.emx.EMXClient;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Arrays;
+
+public class EmxClientTest
+{
+	@Test
+	public void zipITTest() throws Exception
+	{
+		HttpClient httpClient = new TestHttpClient();
+		MolgenisRestApiClient client = new MolgenisRestApiClient(httpClient, new URI(""));
+		EMXClient emxClient = new EMXClient(client);
+
+		File actual = File.createTempFile("download", ".zip");
+		emxClient.downloadEMX(Arrays.asList("org_molgenis_test_TypeTest"), actual.toPath(), true, true);
+		File expected = new File(Resources.getResource("integration/download.zip").getPath());
+		ZipFileAssert.assertEquals(expected, actual);
+	}
+}
