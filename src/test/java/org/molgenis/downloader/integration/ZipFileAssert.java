c3cf7c445a66108e6fa489d03968a5b47cfe2156
bartcharbon <bart.charbon@gmail.com>
2017-01-26 10:25:09 +0100

    remove unused if statement containing TODO

--- src/test/java/org/molgenis/downloader/integration/ZipFileAssert.java
+++ src/test/java/org/molgenis/downloader/integration/ZipFileAssert.java
@@ -38,19 +38,22 @@ import junit.framework.AssertionFailedError;
 /**
  * Compare the contents of 2 zip files.
  */
-public class ZipFileAssert {
-	private ZipFileAssert() {
+public class ZipFileAssert
+{
+	private ZipFileAssert()
+	{
 	}
 
 	static final int BUFFER_SIZE = 2048;
 
-	protected static void equals(
-			TreeMap<String, ByteArrayOutputStream> file1,
-			TreeMap<String, ByteArrayOutputStream> file2) {
+	protected static void equals(TreeMap<String, ByteArrayOutputStream> file1,
+			TreeMap<String, ByteArrayOutputStream> file2)
+	{
 		Set<String> listFile1 = file1.keySet();
 		Assert.assertEquals("not the same number of files in zip:", listFile1.size(), file2.keySet().size());
 
-		for (String fileName : listFile1) {
+		for (String fileName : listFile1)
+		{
 			// extract the contents for both
 			ByteArrayOutputStream contain2 = file2.get(fileName);
 			ByteArrayOutputStream contain1 = file1.get(fileName);
@@ -58,21 +61,16 @@ public class ZipFileAssert {
 			assertNotNull(fileName + " not found in 2nd zip", contain2);
 			// no need to check for contain1. The key come from it
 
-			if ((fileName.endsWith(".xml")) || fileName.endsWith(".rels")) {
-				// we have a xml file
-				// TODO
-				// YK: the original OpenXML4J version attempted to compare xml using xmlunit (http://xmlunit.sourceforge.net),
-				// but POI does not depend on this library
-			} else {
-				// not xml, may be an image or other binary format
-				Assert.assertEquals(fileName + " does not have the same size in both zip:", contain2.size(), contain1.size());
-				assertArrayEquals("contents differ", contain1.toByteArray(), contain2.toByteArray());
-			}
+			// not xml, may be an image or other binary format
+			Assert.assertEquals(fileName + " does not have the same size in both zip:", contain2.size(),
+					contain1.size());
+			assertArrayEquals("contents differ", contain1.toByteArray(), contain2.toByteArray());
+
 		}
 	}
 
-	protected static TreeMap<String, ByteArrayOutputStream> decompress(
-			File filename) throws IOException {
+	protected static TreeMap<String, ByteArrayOutputStream> decompress(File filename) throws IOException
+	{
 		// store the zip content in memory
 		// let s assume it is not Go ;-)
 		TreeMap<String, ByteArrayOutputStream> zipContent = new TreeMap<String, ByteArrayOutputStream>();
@@ -90,14 +88,16 @@ public class ZipFileAssert {
 		/* Processing entries of the zip file */
 		ZipEntry entree;
 		int count;
-		while ((entree = zis.getNextEntry()) != null) {
+		while ((entree = zis.getNextEntry()) != null)
+		{
 
 			/* Create a array for the current entry */
 			ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
 			zipContent.put(entree.getName(), byteArray);
 
 			/* copy in memory */
-			while ((count = zis.read(data, 0, BUFFER_SIZE)) != -1) {
+			while ((count = zis.read(data, 0, BUFFER_SIZE)) != -1)
+			{
 				byteArray.write(data, 0, count);
 			}
 			/* Flush the buffer */
@@ -114,25 +114,26 @@ public class ZipFileAssert {
 	 * Asserts that two files are equal. Throws an <tt>AssertionFailedError</tt>
 	 * if they are not.
 	 * <p>
-	 *
 	 */
-	public static void assertEquals(File expected, File actual) {
+	public static void assertEquals(File expected, File actual)
+	{
 		assertNotNull(expected);
 		assertNotNull(actual);
 
-		assertTrue("File does not exist [" + expected.getAbsolutePath()
-				+ "]", expected.exists());
-		assertTrue("File does not exist [" + actual.getAbsolutePath()
-				+ "]", actual.exists());
+		assertTrue("File does not exist [" + expected.getAbsolutePath() + "]", expected.exists());
+		assertTrue("File does not exist [" + actual.getAbsolutePath() + "]", actual.exists());
 
 		assertTrue("Expected file not readable", expected.canRead());
 		assertTrue("Actual file not readable", actual.canRead());
 
-		try {
+		try
+		{
 			TreeMap<String, ByteArrayOutputStream> file1 = decompress(expected);
 			TreeMap<String, ByteArrayOutputStream> file2 = decompress(actual);
 			equals(file1, file2);
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			throw new AssertionFailedError(e.toString());
 		}
 	}
